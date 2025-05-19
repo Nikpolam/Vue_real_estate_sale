a127a6ed12d256c9987f5723318f2926cb8c6107
Nikpolam <125681419+Nikpolam@users.noreply.github.com>
2025-05-19 20:56:46 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ npm run build
 # 3. Вернуться и скопировать результат сборки в Django
 cd ..
 rm -rf static/assets
-cp -r Vue_real_estate_sale-main/dist/assets static/
+cp -r dist/assets static/
 
 # 4. Копировать index.html в templates
 mkdir -p templates
-cp Vue_real_estate_sale-main/dist/index.html templates/index.html
+cp dist/index.html templates/index.html
 
 python manage.py collectstatic --noinput
